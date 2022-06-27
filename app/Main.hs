a2730057f7067ad382dd815b0391d04c53457396
sarasuartha09 <sakanayatta@gmail.com>
2022-06-27 21:33:21 +0800

    update menu

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,43 @@
 module Main where
 
+import System.IO (hFlush, stdout)
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout
+    getLine
+
+runProgram messages = do
+    putStrLn "\n\n\n=============== Crypto Arbitrage Scanner  ==============="
+    putStrLn "       (a) Show all exchange"
+    putStrLn "       (b) Setup favorite coin"
+    putStrLn "       (c) Setup program"
+    putStrLn "       (d) View Scanner"
+    putStrLn "       (e) Exit program"
+    putStrLn "========================================================="
+    choice <- prompt "Input choice: "
+    case choice of
+        "a" -> do
+            empty <- prompt "Press enter to go back"
+            runProgram messages
+        "b" -> do
+            empty <- prompt "Press enter to go back"
+            runProgram messages
+        "c" -> do
+            empty <- prompt "Press enter to go back"
+            runProgram messages
+        "d" -> do
+            empty <- prompt "Press enter to go back"
+            runProgram messages
+        "e" -> do
+            putStrLn "Exiting program..."
+            putStrLn "Goodbye!"
+        _ -> do
+            empty <- prompt "Wrong input! Press enter to try again."
+            runProgram messages
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    runProgram []
